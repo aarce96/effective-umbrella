7b66baf4223f1d696c91f9bbf6e0aa91eeaad7e0
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-10 16:37:43 -0500

    fix bug

--- db/user-schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `management`.
-`user` ( 
-    `id` INT NOT NULL AUTO_INCREMENT , 
-    `first_name` VARCHAR(45) NOT NULL , 
-    `last_name` VARCHAR(45) NOT NULL , 
-    `email` VARCHAR(45) NOT NULL , 
-    `comments` TEXT NOT NULL , 
-    `status` VARCHAR(10) NOT NULL DEFAULT 'active' , 
-    PRIMARY KEY (`id`)) ENGINE = InnoDB;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+// const mysql = require("mysql");
 const exphbs = require("express-handlebars");
 
 require("dotenv").config();
